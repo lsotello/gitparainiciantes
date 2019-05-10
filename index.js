f50a583091431f22228f25f86e3da3e66f318e7e
Luciano Sotello <lsotello@gmail.com>
2019-05-10 15:39:33 -0300

    Nova rota adicionada

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req,res) =>{
     res.status(200).send({message:'Olá mundo esse é meu novo repositorio clonado!'});
 })
 
+app.get('/ola',(req,res) =>{
+    res.status(200).send({message:'Olá!'});
+})
+
 app.listen(4001,()=>{
     console.log('API rodando na porta 4001');
 })
